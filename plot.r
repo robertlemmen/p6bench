db08324a84f3aa3e88f90c37f1cd58dae4627168
Robert Lemmen <robertle@semistable.com>
2018-09-17 09:13:23 +0200

    add dots to diagram

--- plot.r
+++ plot.r
@@ -9,11 +9,14 @@ data$mean = rowMeans(data[c('V3', 'V4')])
 
 plot(data$V1, data$V3, ylim=yrange, type="l", col="blue", 
     xaxt="n", ylab="Benchmark Duration [s]", xlab="Date of Release", axes=FALSE)
+points(data$V1, data$V3, ylim=yrange, col="blue")
 lines(data$V1, data$V4, ylim=yrange, col="green")
+points(data$V1, data$V4, ylim=yrange, col="green")
 legend("topright", legend=c("01-logmunge", "02-gauss-elimi", "total"), fill=c("blue", "green", "black"),
     cex=0.8, inset=c(0.01,0.01))
 #lines(data$V1, data$V5, ylim=yrange, col="orange")
 lines(data$V1, data$mean, ylim=yrange, col="black", lwd=3)
+points(data$V1, data$mean, ylim=yrange, col="black")
 
 axis(2)
 r <- round(as.POSIXct(range(data$V1), origin="1970-01-01"), "days")
@@ -26,6 +29,7 @@ svg("bench-broken.svg",width=11.32,height=7)
 
 plot(data$V1, data$V5, ylim=yrange, type="l", col="orange", 
     xaxt="n", ylab="Benchmark Duration [s]", xlab="Date of Release", axes=FALSE)
+points(data$V1, data$V5, ylim=yrange, col="orange")
 legend("topright", legend=c("03-backend"), fill=c("orange"),
     cex=0.8, inset=c(0.01,0.01))
 
