957e12f6eee193c3424076f861f29704405e2d5d
Robert Lemmen <robertle@semistable.com>
2018-09-23 18:39:30 +0200

    run benchmark multiple times

--- plot.r
+++ plot.r
@@ -4,16 +4,16 @@ svg("bench.svg",width=11.32,height=7)
 
 data <- read.table("out", na.strings="??")
 
-yrange <- range(data$V3, data$V4, data$V5, 0,  na.rm = TRUE)
-data$mean = rowMeans(data[c('V3', 'V4', 'V5')])
+yrange <- range(data$V3, data$V6, data$V9, 0,  na.rm = TRUE)
+data$mean = rowMeans(data[c('V3', 'V6', 'V9')])
 
 plot(data$V1, data$V3, ylim=yrange, type="l", col="blue", 
     xaxt="n", ylab="Benchmark Duration [s]", xlab="Date of Release", axes=FALSE)
 points(data$V1, data$V3, ylim=yrange, col="blue")
-lines(data$V1, data$V4, ylim=yrange, col="green")
-points(data$V1, data$V4, ylim=yrange, col="green")
-lines(data$V1, data$V5, ylim=yrange, col="orange")
-points(data$V1, data$V5, ylim=yrange, col="orange")
+lines(data$V1, data$V6, ylim=yrange, col="green")
+points(data$V1, data$V6, ylim=yrange, col="green")
+lines(data$V1, data$V9, ylim=yrange, col="orange")
+points(data$V1, data$V9, ylim=yrange, col="orange")
 legend("topright", legend=c("01-logmunge", "02-gauss-elimi", "03-mandelbrot", "average"), fill=c("blue", "green", "orange", "black"),
     cex=0.8, inset=c(0.01,0.01))
 #lines(data$V1, data$V5, ylim=yrange, col="orange")
@@ -29,9 +29,9 @@ dev.off()
 
 svg("bench-broken.svg",width=11.32,height=7)
 
-plot(data$V1, data$V6, ylim=yrange, type="l", col="red", 
+plot(data$V1, data$V12, ylim=yrange, type="l", col="red", 
     xaxt="n", ylab="Benchmark Duration [s]", xlab="Date of Release", axes=FALSE)
-points(data$V1, data$V6, ylim=yrange, col="red")
+points(data$V1, data$V12, ylim=yrange, col="red")
 legend("topright", legend=c("04-backend"), fill=c("red"),
     cex=0.8, inset=c(0.01,0.01))
 
